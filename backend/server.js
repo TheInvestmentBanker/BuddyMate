33dd275ce56c8620208bbdcb657c30f9d3546246
TheInvestmentBanker <strandedincosmos@gmail.com>
2025-03-06 22:34:46 +0530

    Update CORS origin to https://buddymate.space

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
